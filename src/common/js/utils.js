fbeff897b717508b2e1a239db47aa7c9486d6248
WangMinan <wangminan0811@hotmail.com>
2022-11-27 17:06:03 +0800

    重要更新:修正了后端服务器指向不正确的问题

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -4308,7 +4308,7 @@ export const prefix = (url) => {
   if (url && url.startsWith('http')) {
     return url
   } else {
-    url = `http://backend-api-01.newbee.ltd${url}`
+    url = `http://127.0.0.1:28019${url}`
     return url
   }
 }
